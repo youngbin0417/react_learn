38f0a3b2592f2fe5c27af052ab8c920eecad2e0d
Youngbin Ko <66235935+youngbin0417@users.noreply.github.com>
2026-05-20 11:52:07 +0900

    0520 오전

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Header from './components/header'
+
+/*const fruits = ['사과', '바나나', '포도'];
+<ul>
+      <h1 className='test'>과일 목록</h1>
+      <p style={{ color: "red", fontSize: 24 }}>맛있어요</p>
+      {fruits.map(f => <li>{f}</li>)}
+    </ul> */
+
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,8 @@
+
+function Header() {
+  return (
+    <h1>⌨️ 미니 게시판</h1>
+  )
+}
+
+export default Header;
